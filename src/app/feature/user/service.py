aa56af37ba3905301db7d067d92d5d387e7830a6
Davron <xdavron@mail.ru>
2026-06-13 00:12:13 +0500

    added function for getting user profile

---
+++ src/app/feature/user/service.py
@@ -0,0 +1,9 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.feature.user.models import User
+
+
+async def get_user_profile(session: AsyncSession, user_id: int) -> User | None:
+    result = await session.execute(select(User).where(User.id == user_id))
+    return result.scalar_one_or_none()
