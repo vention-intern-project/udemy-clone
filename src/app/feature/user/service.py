4f318a9479570346a86ea9b358661da5707be178
Abdumalik <aabdusalomov533@gmail.com>
2026-06-14 15:15:37 +0500

    [ADT-27] User registration and role management: added register_user function to src/app/feature/user/services.py file and get_user_by_email to src/app/feature/user/repository.py file

--- src/app/feature/user/service.py
+++ src/app/feature/user/service.py
@@ -1,8 +1,36 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.feature.user.models import User
-from app.feature.user.repository import get_user_by_id
+from app.feature.user.repository import get_user_by_id, get_user_by_email
+from app.feature.user.schemas import UserRegister
 
 
 async def get_user_profile(session: AsyncSession, user_id: int) -> User | None:
     return await get_user_by_id(session, user_id)
+
+
+async def register_user(
+    session: AsyncSession,
+    user_data: UserRegister,
+):
+
+    existing_user = await get_user_by_email(session, user_data.email)
+
+    if existing_user:
+        raise ValueError(
+            "User already exists"
+        )
+
+    user = User(
+        email=user_data.email,
+        name=user_data.name,
+        surname=user_data.surname,
+        password=user_data.password,
+        role=user_data.role,
+    )
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    return user
