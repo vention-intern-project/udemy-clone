7e4b1a14c31af55b45bde2522637c621e560b0c1
Davron <xdavron@mail.ru>
2026-06-15 14:16:16 +0500

    added login_user() function in service layer for login and creating access token

--- src/app/feature/user/service.py
+++ src/app/feature/user/service.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.security import create_access_token
 from app.feature.user.models import User
 from app.feature.user.repository import get_user_by_email, get_user_by_id
-from app.feature.user.schemas import UserRegister
+from app.feature.user.schemas import UserLogin, UserRegister
 
 
 async def get_user_profile(session: AsyncSession, user_id: int) -> User | None:
@@ -35,3 +35,12 @@ async def register_user(
     token = create_access_token(data={"id": str(user.id)})
 
     return {"user": user, "access_token": token, "token_type": "bearer"}
+
+
+async def login_user(session: AsyncSession, user_data: UserLogin) -> str:
+    user = await get_user_by_email(session, user_data.email)
+
+    if user is None or user.password != user_data.password:
+        raise ValueError("Invalid email or password")
+
+    return create_access_token(data={"id": str(user.id)})
