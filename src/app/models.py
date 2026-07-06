3a86c08c39d7cfda420ddeb311b30b5d7f516f13
Abdumalik <aabdusalomov533@gmail.com>
2026-07-06 19:23:34 +0500

    [ADT-45] Progress tracking: added LessonProgess table to app/models.py file

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,3 @@
 from app.feature.course.models import Course, Lesson  # noqa: F401
-from app.feature.enrollment.models import Enrollment  # noqa: F401
+from app.feature.enrollment.models import Enrollment, LessonProgress  # noqa: F401
 from app.feature.user.models import User  # noqa: F401
