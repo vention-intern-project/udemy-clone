5192e0bd29c49f9559fc2d2fe09ce64b414390b0
Abdumalik <aabdusalomov533@gmail.com>
2026-07-08 17:24:57 +0500

    [ADT-79] Cart models: fixed ruff errors for the second time

--- src/alembic/versions/6b1855bea1d5_creating_cart_and_cartitem_tables.py
+++ src/alembic/versions/6b1855bea1d5_creating_cart_and_cartitem_tables.py
@@ -5,17 +5,14 @@ Revises: afc42138d510
 Create Date: 2026-07-08 17:23:24.564405
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
-revision: str = '6b1855bea1d5'
-down_revision: Union[str, Sequence[str], None] = 'afc42138d510'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "6b1855bea1d5"
+down_revision: str | Sequence[str] | None = "afc42138d510"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
