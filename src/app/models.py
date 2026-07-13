19191b152ada4b15d4e3f9b3041d176f3457035c
Davron <xdavron@mail.ru>
2026-07-13 12:20:52 +0500

    fixed empty alembic migration of cart and cart item

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,4 @@
+from app.feature.cart.models import Cart, CartItem  # noqa: F401
 from app.feature.course.models import Course, Lesson  # noqa: F401
 from app.feature.enrollment.models import Enrollment, LessonProgress  # noqa: F401
 from app.feature.user.models import User  # noqa: F401
