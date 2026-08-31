699f831751047c2bb204e306c56990207bc56715
Abdumalik <aabdusalomov533@gmail.com>
2026-08-31 16:29:08 +0500

    adding frontend domain

--- src/app/main.py
+++ src/app/main.py
@@ -32,10 +32,7 @@ admin = create_admin(app)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:4173",
+        "https://learnhuubfront.duckdns.org",
     ],
     allow_credentials=True,
     allow_methods=["*"],
